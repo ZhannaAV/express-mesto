b330fb384b826520bf008320b9a84e6c69847a5a
Zhanna Anikanova <whiteredfoxx@gmail.com>
2021-06-09 01:12:50 +0300

    feat:проверка прав на удаление карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,7 +3,7 @@ const Card = require('../models/card');
 module.exports.getCards = (req, res) => {
   Card.find({})
     .orFail(() => new Error('NotFound'))
-    // .populate(['owner', 'likes']) что-то не так с подключением этого метода
+  // .populate(['owner', 'likes']) что-то не так с подключением этого метода
     .then((cards) => res.status(200).send(cards))
     .catch((err) => {
       if (err.message === 'NotFound') return res.status(404).send({ message: 'Объект не найден' });
@@ -15,7 +15,7 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const userId = req.user._id;
   Card.create({ name, link, owner: userId })
-    // .populate(['owner', 'likes'])
+  // .populate(['owner', 'likes'])
     .then((card) => res.status(200).send(card))
     .catch((err) => {
       if (err.name === 'ValidatorError') return res.status(400).send({ message: 'Переданы некорректные данные' });
@@ -24,11 +24,19 @@ module.exports.createCard = (req, res) => {
 };
 
 module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId)
+  const { cardId } = req.params;
+  const userId = req.user._id;
+  Card.findById(cardId)
     .orFail(() => new Error('NotFound'))
-    .then(() => res.status(200).send({ message: 'Пост удален' }))
+    .then((card) => {
+      if (card.owner === userId) return card._id;
+      return Promise.reject(new Error('Нет прав для удаления поста'));
+    })
+    .then((id) => Card.findByIdAndRemove(id)
+      .then(() => res.status(200).send({ message: 'Пост удален' })))
     .catch((err) => {
       if (err.name === 'CastError') return res.status(400).send({ message: 'Переданы некорректные данные' });
+      if (err.message === 'Нет прав для удаления поста') return res.status(403).send({ message: err.message });
       if (err.message === 'NotFound') return res.status(404).send({ message: 'Объект не найден' });
       return res.status(500).send({ message: 'Произошла ошибка' });
     });
@@ -38,7 +46,7 @@ module.exports.likeCard = (req, res) => {
   const userId = req.user._id;
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: userId } }, { new: true })
     .orFail(() => new Error('NotFound'))
-    // .populate(['owner', 'likes'])
+  // .populate(['owner', 'likes'])
     .then((card) => res.status(200).send(card))
     .catch((err) => {
       if (err.name === 'CastError') return res.status(400).send({ message: 'Переданы некорректные данные' });
@@ -51,7 +59,7 @@ module.exports.dislikeCard = (req, res) => {
   const userId = req.user._id;
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: userId } })
     .orFail(() => new Error('NotFound'))
-    // .populate(['owner', 'likes'])
+  // .populate(['owner', 'likes'])
     .then((card) => res.status(200).send(card))
     .catch((err) => {
       if (err.name === 'CastError') return res.status(400).send({ message: 'Переданы некорректные данные' });
